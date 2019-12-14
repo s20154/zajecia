ede467a74e569ab463ce3fd5bc5767f813928241
Damian Kijańczuk <kijanczukdamian@gmail.com>
2019-12-14 11:52:05 +0100

    auto

--- 2019-12-14_PRG1/STRINGI.cpp
+++ 2019-12-14_PRG1/STRINGI.cpp
@@ -14,13 +14,21 @@ int main()
 	// .erase(10,14)
 
 	string napis;
+	cout <<"Podaj napis:\t"; cin >> napis;
+
+	int i = 0;
+	int j = napis.length() - 1;
 	
-	do{
-		
-		
-	}while()
-
-
+	while(i<j){
+		if(napis[i] != napis[j]){
+			cout <<"Niejest";
+			return -1;
+		}
+		++i;
+		--j;
+	}
+	
+	cout <<"Jest";
     return 0;
 }
 
